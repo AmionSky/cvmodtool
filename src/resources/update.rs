68f080c791a104c2fc9ef41452e877491b1ebca2
Csányi István <csanyistvan02@gmail.com>
2022-02-09 18:53:26 +0100

    Update "updater"

--- src/resources/update.rs
+++ src/resources/update.rs
@@ -1,12 +1,9 @@
 use crate::colored::info;
-use std::error::Error;
 use std::fs::File;
 use std::path::PathBuf;
-use std::sync::Arc;
 use updater::extract::{self, ExtractResult};
 use updater::provider::{Asset, DownloadResult, Provider};
-use updater::update::{StepAction, UpdateProcedure, UpdateStep};
-use updater::{Progress, Version};
+use updater::{State, StepAction, StepResult, Updater, Version};
 
 pub struct UpdateData {
     pub provider: Box<dyn Provider>,
@@ -37,107 +34,89 @@ impl UpdateData {
     }
 }
 
-pub struct StepCheckVersion;
-impl UpdateStep<UpdateData> for StepCheckVersion {
-    fn exec(&self, data: &mut UpdateData, _: &Arc<Progress>) -> Result<StepAction, Box<dyn Error>> {
-        info(&format!(
-            "Checking for latest version via {}",
-            data.provider.name()
-        ));
-        data.provider.fetch()?;
+fn step_check_version(state: &mut State, data: &mut UpdateData) -> StepResult {
+    state.set_label("Checking for latest version...".into());
 
-        let latest = data.provider.latest()?;
-        if latest <= data.version {
-            info("Resources are up-to-date");
-            return Ok(StepAction::Complete);
-        }
+    info(&format!(
+        "Checking for latest version via {}",
+        data.provider.name()
+    ));
+    data.provider.fetch()?;
 
-        let asset = data.provider.asset(&latest, &data.asset_name)?;
-        info(&format!("Updating to v{} (from v{})", latest, data.version));
-
-        // Update data
-        data.version = latest;
-        data.asset = Some(asset);
-
-        Ok(StepAction::Continue)
+    let latest = data.provider.latest()?;
+    if latest <= data.version {
+        info("Resources are up-to-date");
+        return Ok(StepAction::Complete);
     }
 
-    fn label(&self, _: &UpdateData) -> String {
-        "Checking for latest version...".to_string()
-    }
+    let asset = data.provider.asset(&latest, &data.asset_name)?;
+    info(&format!("Updating to v{} (from v{})", latest, data.version));
+
+    // Update data
+    data.version = latest;
+    data.asset = Some(asset);
+
+    Ok(StepAction::Continue)
 }
 
-pub struct StepDownload;
-impl UpdateStep<UpdateData> for StepDownload {
-    fn exec(
-        &self,
-        data: &mut UpdateData,
-        progress: &Arc<Progress>,
-    ) -> Result<StepAction, Box<dyn Error>> {
-        info(&format!("Downloading resources v{}", &data.version));
+fn step_download(state: &mut State, data: &mut UpdateData) -> StepResult {
+    state.set_label(format!(
+        "Downloading {:.2} MB",
+        data.asset.as_ref().unwrap().size() as f64 / 1_000_000.0
+    ));
 
-        let dl_result = data.asset.as_ref().unwrap().download(progress.clone());
+    info(&format!("Downloading resources v{}", &data.version));
 
-        let file = match dl_result {
-            DownloadResult::Complete(file) => file,
-            DownloadResult::Cancelled => return Ok(StepAction::Cancel),
-            DownloadResult::Error(e) => return Err(format!("Asset download failed: {}", e).into()),
-        };
+    let dl_result = data
+        .asset
+        .as_ref()
+        .unwrap()
+        .download(state.progress().clone());
 
-        data.file = Some(file);
-        info("Download finished!");
+    let file = match dl_result {
+        DownloadResult::Complete(file) => file,
+        DownloadResult::Cancelled => return Ok(StepAction::Cancel),
+        DownloadResult::Error(e) => return Err(format!("Asset download failed: {}", e).into()),
+    };
 
-        Ok(StepAction::Continue)
-    }
+    data.file = Some(file);
+    info("Download finished!");
 
-    fn label(&self, data: &UpdateData) -> String {
-        format!(
-            "Downloading {:.2} MB",
-            data.asset.as_ref().unwrap().size() as f64 / 1_000_000.0
-        )
-    }
+    Ok(StepAction::Continue)
 }
 
-pub struct StepInstall;
-impl UpdateStep<UpdateData> for StepInstall {
-    fn exec(
-        &self,
-        data: &mut UpdateData,
-        progress: &Arc<Progress>,
-    ) -> Result<StepAction, Box<dyn Error>> {
-        info("Starting install");
+fn step_install(state: &mut State, data: &mut UpdateData) -> StepResult {
+    state.set_label("Installing...".into());
 
-        // (Re)Create install folder
-        let install_path = &data.directory;
-        if install_path.is_dir() {
-            std::fs::remove_dir_all(&install_path)?;
-        }
-        std::fs::create_dir(&install_path)?;
+    info("Starting install");
 
-        // Unpack asset
-        if extract::asset(
-            data.asset.as_ref().unwrap().name(),
-            data.file.take().unwrap(),
-            &install_path,
-            progress.clone(),
-        )? == ExtractResult::Cancelled
-        {
-            return Ok(StepAction::Cancel);
-        }
+    // (Re)Create install folder
+    let install_path = &data.directory;
+    if install_path.is_dir() {
+        std::fs::remove_dir_all(&install_path)?;
+    }
+    std::fs::create_dir(&install_path)?;
 
-        data.success = true;
-        Ok(StepAction::Continue)
+    // Unpack asset
+    if extract::asset(
+        data.asset.as_ref().unwrap().name(),
+        data.file.take().unwrap(),
+        &install_path,
+        state.progress().clone(),
+    )? == ExtractResult::Cancelled
+    {
+        return Ok(StepAction::Cancel);
     }
 
-    fn label(&self, _: &UpdateData) -> String {
-        "Installing...".to_string()
-    }
+    data.success = true;
+    Ok(StepAction::Continue)
 }
 
-pub fn create(data: UpdateData) -> UpdateProcedure<UpdateData> {
-    let mut procedure = UpdateProcedure::new("Resource Updater".to_string(), data);
-    procedure.add_step(Box::new(StepCheckVersion));
-    procedure.add_step(Box::new(StepDownload));
-    procedure.add_step(Box::new(StepInstall));
-    procedure
+pub fn create(data: UpdateData) -> Updater<UpdateData> {
+    let mut updater = Updater::new(data);
+    updater.set_title("Resource Updater".into());
+    updater.add_step(step_check_version);
+    updater.add_step(step_download);
+    updater.add_step(step_install);
+    updater
 }
